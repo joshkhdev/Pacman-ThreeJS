c6faa1b6ecac29ca717aa7718d384085539052e8
Josh Khrustalev <khrustalevjosh@gmail.com>
2021-05-25 19:51:35 +0300

    Ver. 4 - Pacman contour + icon

--- scripts/main.js
+++ scripts/main.js
@@ -20,26 +20,26 @@ renderer.setSize(width, height);
 var controls = new OrbitControls(camera, renderer.domElement);
 document.body.appendChild(renderer.domElement);
 
-// создание геометрии
-var geometry = new THREE.BoxGeometry(cube_size, cube_size, cube_size);
-var material = new THREE.MeshBasicMaterial({ color: 0x000000 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-
-// добавление контуров для куба
-const edges = new THREE.EdgesGeometry( geometry );
-const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color:  0xfafafa } ) );
-line.material.linewidth = 2; // optional
-scene.add( line );
-
-// изменение цвета фона 
-scene.background = new THREE.Color( 0xD0D0D0 );
+// изменение цвета фона
+scene.background = new THREE.Color(0xd0d0d0);
 
 // определение положения камеры
 camera.position.set(0, 100, 1000);
 controls.update();
 
+// создание куба
+var geometry = new THREE.BoxGeometry(cube_size, cube_size, cube_size);
+var material = new THREE.MeshBasicMaterial({ color: 0x000000 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// добавление контуров для куба
+let edges = new THREE.EdgesGeometry(geometry);
+let contour = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color:  0xfafafa }));
+contour.material.linewidth = 2;
+scene.add(contour);
+
+// создание менеджера загруки моделей
 const manager = new THREE.LoadingManager();
 manager.onStart = function (url, itemsLoaded, itemsTotal) {
     console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
@@ -49,7 +49,6 @@ manager.onLoad = function () {
     console.log('Loading complete!');
 };
 
-
 manager.onProgress = function (url, itemsLoaded, itemsTotal) {
     console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
 };
@@ -58,15 +57,22 @@ manager.onError = function (url) {
     console.log('There was an error loading ' + url);
 };
 
-
+// загрузка модели пакмана
 const loader = new GLTFLoader(manager);
 loader.load('./models/pacman.glb', function (gltf) {
+    // создание геометрии и mesh (модели) пакмана
     let geometry = gltf.scene.children[0].geometry;
     let material = new THREE.MeshBasicMaterial({ color: 0xffff33 });
     let pacman = new THREE.Mesh(geometry, material);
     pacman.scale.set(pacman_size, pacman_size, pacman_size);
     pacman.rotateY(-Math.PI / 2);
-    pacman.position.set(0, 0, cube_size / 2 + pacman_size / 2);
+    pacman.position.set(0, 0, cube_size / 2 + pacman_size);
+
+    // создание контура для пакмана
+    let edges = new THREE.EdgesGeometry(pacman.geometry);
+    let contour = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color:  0x00ff00 }));
+    contour.material.linewidth = 2;
+    pacman.add(contour);
 
     scene.add(pacman);
 }, undefined, function (error) {
